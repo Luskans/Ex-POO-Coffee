aa9dbf8886c0b6d37c889b52d9a2732f128d3b57
Luskans <sylvain.migne@gmail.com>
2023-07-25 22:02:59 +0200

    add visual for coffee machine

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coffee Machine</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="coffee-header">
+            <div class="coffee-header__power">
+                <div class="coffee-header__power-one">
+                    <div class="coffee-header__power-two"></div>
+                </div>
+            </div>
+            <div class="coffee-header__buttons coffee-header__button-one"></div>
+            <div class="coffee-header__buttons coffee-header__button-two"></div>
+            <div class="coffee-header__display"></div>
+            <div class="coffee-header__sugar"></div>
+            <div class="coffee-header__details"></div>
+            <div class="coffee-header__screen"></div>
+            <div class="coffee-header__coins"></div>           
+        </div>
+        <div class="coffee-medium">
+            <div class="coffe-medium__exit"></div>
+            <div class="coffee-medium__arm"></div>
+            <div class="coffee-medium__liquid"></div>
+            <div class="coffee-medium__smoke coffee-medium__smoke-one"></div>
+            <div class="coffee-medium__smoke coffee-medium__smoke-two"></div>
+            <div class="coffee-medium__smoke coffee-medium__smoke-three"></div>
+            <div class="coffee-medium__smoke coffee-medium__smoke-for"></div>
+            <div class="coffee-medium__cup"></div>
+        </div>
+        <div class="coffee-footer"></div>
+    </div>
+    <div class="coffee-tube"></div>
+    <div class="coffee-menu">
+        <div class="coffee-menu__buttons coffee-menu__button-one"></div>
+        <div class="coffee-menu__buttons coffee-menu__button-two"></div>
+        <div class="coffee-menu__buttons coffee-menu__button-tree"></div>
+        <div class="coffee-menu__buttons coffee-menu__button-four"></div>
+        <div class="coffee-menu__labels coffee-menu__label-one">
+            <p> Expresso : 50 cts </p>
+        </div>
+        <div class="coffee-menu__labels coffee-menu__label-two">
+            <p> Cappuccino : 200 cts </p>
+        </div>
+        <div class="coffee-menu__labels coffee-menu__label-tree">
+            <p> Mokaccino : 180 cts </p>
+        </div>
+        <div class="coffee-menu__labels coffee-menu__label-four">
+            <p> Chocolat : 100 cts </p>
+        </div>
+    </div>
+</body>
+</html>
+
